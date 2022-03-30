9f212454ea3c3955a71e3319d1544a617ce4fde7
aneervan <paulanirban1997@gmail.com>
2022-03-30 21:58:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+from transformers import pipeline
+import pandas as pd
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+
+def predict():
+    """Predict answer to the user question."""
+    questions = [str(x) for x in request.form.values()]
+    question = str(questions[0])
+    # answer = predict(question)
+    reader = pipeline("question-answering")
+  # question = "What does the customer want?"
+    df = pd.read_excel('pdf_extract_test.xlsx')
+    text_list = df.content.tolist()
+    text = " ".join(text_list)
+    outputs = reader(question=question, context=text)
+    op_df = pd.DataFrame.from_records([outputs])
+    answer = str(op_df['answer'].iloc[0])
+    return render_template('index.html', prediction_text='Predicted answer for "'+question+'" :  {}'.format(answer))
+    # try:
+    #     fahrenheit = float(question) * 9 / 5 + 32
+    #     fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+    #     return str(fahrenheit)
+    # except ValueError:
+    #     return "invalid input"
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8282, debug=True)
